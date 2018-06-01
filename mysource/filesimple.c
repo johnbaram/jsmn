7ea3199503ca177f14d7cfadf154077c66759126
Wind Shin <johnbaram@naver.com>
2018-06-01 16:15:49 +0900

    example 8 fixed

--- mysource/filesimple.c
+++ mysource/filesimple.c
@@ -77,8 +77,8 @@ void example8List(char *jsonstr, jsmntok_t *t, int tokcount, int *nameTokIndex){
 	printf("***** Object List *******\n");
 	for (i = 0; i < tokcount; i++) { //repeats by the number of the token
 			//if (jsoneq(jsonstr, &t[i], "name") == 0) { //true if the token is "name"
-			if(t[i].type==JSMN_STRING && t[i].size>0){ //토큰의 타입의 스트링이면서 토큰의 parent가 전체이면(depth가 1이다.)
-				printf("[Name %d] %.*s\n", e, t[i+1].end-t[i+1].start, jsonstr + t[i+1].start); //print the value
+			if(t[i].type==JSMN_OBJECT && t[i].parent==0){ //토큰의 타입의 스트링이면서 토큰의 parent가 전체이면(depth가 1이다.)
+				printf("[Name %d] %.*s\n", e, t[i+2].end-t[i+2].start, jsonstr + t[i+2].start); //print the value
 				e++;
 			}
 		}
@@ -86,11 +86,11 @@ void example8List(char *jsonstr, jsmntok_t *t, int tokcount, int *nameTokIndex){
 }
 
 void printtoken(const char *JSON_STRING, jsmntok_t *t, int count){
-   int i=0;
-   for(i=0; i<count; i++){
-   printf("index: [%2d] start&end{%d, %d} size <%d> type '%d' %.*s\n", i, t[i].start, t[i].end, t[i].size, t[i].type, t[i].end-t[i].start,
-         JSON_STRING + t[i].start);
-      }
+  int i=0;
+  for(i=0; i<count; i++){
+  printf("index: [%2d] start&end{%d, %d} size <%d> parent-%d type '%d' %.*s\n",
+		i, t[i].start, t[i].end, t[i].size, t[i].parent, t[i].type, t[i].end-t[i].start, JSON_STRING + t[i].start);
+	}
 }
 
 void jsonNameList(char *jsonstr, jsmntok_t *t, int tokcount, int *nameTokIndex){
@@ -105,10 +105,10 @@ void jsonNameList(char *jsonstr, jsmntok_t *t, int tokcount, int *nameTokIndex){
 			}
 		}
 	}
-	/*printf("***** Name List *******\n");
+	printf("***** Name List *******\n");
 	for(i=0; i<c; i++){
 		printf("[NAME %d] %s\n", i+1, nameList[i]);
-	}*/
+	}
 	printf("\n");
 }
 
@@ -145,7 +145,7 @@ int main() {
 	jsonNameList(JSON_STRING, t, r, nameTokIndex);
 	//printNameList(JSON_STRING, t, nameTokIndex);
 	//selectNameList(JSON_STRING, t, nameTokIndex);
-	example8List(JSON_STRING, t, r, nameTokIndex);
+	//example8List(JSON_STRING, t, r, nameTokIndex);
 	//example9SelectList(JSON_STRING, t, r, nameTokIndex);
 
 	return EXIT_SUCCESS;
